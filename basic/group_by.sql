e37138293dfd0c7908f9fba4a6b0c1bdd852db17
Anji <zam58mrp@i.softbank.jp>
2026-04-13 18:00:49 +0900

    feat: group_by.sql のQ1, Q2を実装

--- basic/group_by.sql
+++ basic/group_by.sql
@@ -1,3 +1,10 @@
 --Q1: 商品分類ごとにグループ化し、それぞれの「商品分類名」と「その分類に属する行数」を表示してください。
 
+select shohin_bunrui,count(shohin_mei)
+from shohin
+group by shohin_bunrui;
+
 --Q2: 販売単価ごとにグループ化し、その「販売単価」と「その単価に該当する商品数」を表示してください。
+select hanbai_tanka,count(shohin_mei)
+from shohin
+group by hanbai_tanka;
